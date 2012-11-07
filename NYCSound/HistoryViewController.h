da7fee5c427555da1a08f974eedbcc521ce3255b
Ross Cairns <ross@electricglen.com>
2012-11-07 17:55:55 +0000

    Added History table

---
+++ NYCSound/HistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  NYCSound
+//
+//  Created by Ross Cairns on 07/11/2012.
+//  Copyright (c) 2012 COSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryViewController : UITableViewController
+
+@end
