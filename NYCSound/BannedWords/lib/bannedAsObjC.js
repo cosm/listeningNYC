89c205f803e58fa82ad8dddd2f729aec56f2cce6
Ross Cairns <ross@electricglen.com>
2013-02-28 15:23:22 +0000

    Banned words generator

---
+++ NYCSound/BannedWords/lib/bannedAsObjC.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+var lazy = require("lazy");
+
+var bannedWords = [];
+
+new lazy(fs.createReadStream(__dirname+'/../banned.txt'))
+    .lines
+    .forEach(function(line){
+        bannedWords.push(line);
+    })
+    .on('pipe', function() {
+        var output = "";
+        bannedWords.forEach(function (word, index) {
+            output += '@"' + word + '"'; 
+            if (index < bannedWords.length-1) {
+                output += ",";
+            }   
+        });
+        console.log('#pragma once \n\n #define kBANNED_WORDS = @['+output+']');
+    });
